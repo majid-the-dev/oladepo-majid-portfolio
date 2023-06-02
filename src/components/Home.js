ad4735927e3189c8f0395a44c995fc56576320d7
majid-the-dev <contactmajidoladepo@gmail.com>
2023-06-02 08:21:25 -0700

    commit message

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,21 @@ import '../styles/Home.css';
 import heroImage from '../assets/Nerd-pana.png';
 import { Link } from 'react-scroll';
 import { RxDoubleArrowDown } from 'react-icons/rx';
+import { IoClose } from 'react-icons/io5';
 import 'animate.css';
+import { skillsetData } from './skillsetData';
 
 const Home = (props) => {
+
+    const skill = skillsetData.map(skillItem => {
+        return (
+            <div className='skill-item'>
+                <h1>{skillItem.title}</h1>
+                <progress value={skillItem.value} max="100" className='progress'></progress>
+            </div>
+        )
+    })
+
     return (
         <div className='hero-section'>
             <div className='content-box-md'>
@@ -23,19 +35,23 @@ const Home = (props) => {
                                 <button className='skillset' data-bs-toggle="modal" data-bs-target="#staticBackdrop">Explore skillset</button>
                             </div>
                             <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                                <div className="modal-dialog modal-dialog-centered">
+                                <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
                                     <div className="modal-content">
                                         <div className="modal-header">
-                                            <h1 className="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
-                                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            {/* <h1 className="modal-title fs-5" id="staticBackdropLabel">Modal title</h1> */}
+                                            <div className='avatar'>
+                                                <img src="" alt="" />
+                                            </div>
+
+                                            <IoClose type='button' className='close-btn' data-bs-dismiss="modal" aria-label="Close" />
                                         </div>
                                         <div className="modal-body">
-                                            ...
+                                            {skill}
                                         </div>
-                                        <div className="modal-footer">
+                                        {/* <div className="modal-footer">
                                             <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                                             <button type="button" className="btn btn-primary">Understood</button>
-                                        </div>
+                                        </div> */}
                                     </div>
                                 </div>
                             </div>
---
+++ src/components/skillsetData.js
@@ -0,0 +1,63 @@
+export const skillsetData = [
+    {
+        id: 1,
+        title: 'HTML',
+        value: 95
+    },
+    {
+        id: 2,
+        title: 'CSS',
+        value: 95
+    },
+    {
+        id: 3,
+        title: 'CSS Frameworks',
+        value: 95
+    },
+    {
+        id: 4,
+        title: 'JavaScript',
+        value: 90
+    },
+    {
+        id: 5,
+        title: 'React',
+        value: 90
+    },
+    {
+        id: 6,
+        title: 'Next',
+        value: 85
+    },
+    {
+        id: 7,
+        title: 'Node',
+        value: 80
+    },
+    {
+        id: 8,
+        title: 'Express',
+        value: 80
+    },
+    {
+        id: 9,
+        title: 'Mongo DB',
+        value: 85
+    },
+    {
+        id: 10,
+        title: 'Git',
+        value: 90
+    },
+    {
+        id: 11,
+        title: 'Shell scripting',
+        value: 90
+    },
+    {
+        id: 12,
+        title: 'Canva',
+        value: '93'
+    }
+
+]
